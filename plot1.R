cfe416ce31b59961fe5dd745ad730db65079e73c
Patrick Nuguid <phnuguid@up.edu.ph>
2020-11-13 00:05:14 +0800

    use fread instead of read.table

--- plot1.R
+++ plot1.R
@@ -1,5 +1,6 @@
 # Reading and preparing the data
-df <- read.table(file = "~/exdata_data_household_power_consumption/household_power_consumption.txt", sep = ';', header = TRUE)
+library(data.table)
+df <- fread(file = "~/exdata_data_household_power_consumption/household_power_consumption.txt", header = TRUE)
 df2 <- df[which(df$Date == "1/2/2007"),]
 df3 <- df[which(df$Date == "2/2/2007"),]
 final_df <- rbind(df2, df3)
