388593b2b2d00fe8ae17263350a3bb2b163a5168
Patrick Nuguid <phnuguid@up.edu.ph>
2020-11-12 23:36:59 +0800

    add code for reading the data

---
+++ plot1.R
@@ -0,0 +1,4 @@
+# Reading and preparing the data
+df <- read.table(file = "./exdata_data_household_power_consumption/household_power_consumption.txt", sep = ';', header = TRUE)
+df2 <- df[which(df$Date == "1/2/2007"),]
+df3 <- df[which(df$Date == "2/2/2007"),]
