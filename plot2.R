bf3e32074367cd164bc56d689bf4490cb3b5041c
Patrick Nuguid <phnuguid@up.edu.ph>
2021-01-04 14:06:26 +0800

    finish plot2

--- plot2.R
+++ plot2.R
@@ -3,18 +3,18 @@ library(data.table)
 df <- fread(file = "~/exdata_data_household_power_consumption/household_power_consumption.txt", header = TRUE)
 df2 <- df[which(df$Date == "1/2/2007"),]
 df3 <- df[which(df$Date == "2/2/2007"),]
-final_df <- rbind(df2, df3)
+temp_df <- rbind(df2, df3)
 
-# convert Date column from chr to Date dtype
-final_df$Date <- as.Date(final_df$Date, format = "%d/%m/%Y")
-
-# convert Time column from chr to POSIXct dtype
-newtime <- as.POSIXct(strptime(final_df$Time, format = "%H:%M:%S"))
-final_df$Time <- newtime
+# concatenate Date and Time column
+datetime <- paste(temp_df$Date, temp_df$Time, sep=" ")
+samp <- cbind(datetime, temp_df[,-c('Date','Time')])
+DateTime <- as.POSIXct(strptime(samp$datetime, format = "%d/%m/%Y %H:%M:%S"))
+final_df <- cbind(DateTime, samp[,-1])
 
 # convert remaining columns to numeric dtype
-new_cols <- lapply(final_df[,-c("Date", "Time")], FUN = as.numeric)
-samp <- final_df[,1:2]
-df_converted <- cbind.data.frame(samp,new_cols)
+new_cols <- lapply(final_df[,-c("DateTime")], FUN = as.numeric)
+samp2 <- final_df[,1]
+df_converted <- cbind.data.frame(samp2,new_cols)
 
 # plot
+with( df_converted, plot(DateTime, Global_active_power, type='l', ylab='Global Active Power (kilowatts)', xlab='') )
